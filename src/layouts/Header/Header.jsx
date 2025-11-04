b573e9a5c3ea3630222e7f8be7549c7dfc47ec5d
Valeriy <shepotken2012@gmail.com>
2025-11-04 13:54:40 +0400

    добавлен компонент Logo, исправлены мелкие ошибки

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,15 @@
+import Logo from "@/components/Logo"
+import "./Header.scss"
+
+
 const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header className="header">
+      <div className="header__inner container">
+        <Logo className="header__logo" loading="eager" />
+      </div>
+    </header>
+  )
 }
 
 export default Header
