66dc403d5dd116ee6ad23e13f798ff6d901b5c8c
Valeriy <shepotken2012@gmail.com>
2025-11-10 13:28:45 +0400

    Добавлен компонент BurgerButton, завершена работа над шапкой сайта

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,6 +2,7 @@ import Logo from "@/components/Logo"
 import "./Header.scss"
 import classNames from "classnames"
 import Button from "@/components/Button"
+import BurgerButton from "@/components/BurgerButton"
 
 const Header = (props) => {
   const { url } = props
@@ -12,46 +13,58 @@ const Header = (props) => {
     { label: "Subscriptions", href: "/subscriptions" },
   ]
   return (
-    <header className="header">
+    <header className="header" data-js-overlay-menu="">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({ label, href }, index) => {
-              return (
-                <li className="header__menu-item" key={index}>
-                  <a
-                    className={classNames("header__menu-link", {
-                      "is-active": href === url,
-                    })}
-                    title={label}
-                    href={href}
-                  >
-                    {label}
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
+        <dialog
+          className="header__overlay-menu-dialog"
+          data-js-overlay-menu-dialog
+        >
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ label, href }, index) => {
+                return (
+                  <li className="header__menu-item" key={index}>
+                    <a
+                      className={classNames("header__menu-link", {
+                        "is-active": href === url,
+                      })}
+                      title={label}
+                      href={href}
+                    >
+                      {label}
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
 
-        <div className="header__actions">
-          <Button
-            className="header__button"
-            label="Search"
-            iconName="search"
-            isLabelHidden
-            mode="transparent"
-          />
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              iconName="search"
+              isLabelHidden
+              mode="transparent"
+            />
 
-          <Button
-            className="header__button"
-            iconName="notifications"
-            mode="transparent"
-            label="Notifications"
-            isLabelHidden
-          />
-        </div>
+            <Button
+              className="header__button"
+              iconName="notifications"
+              mode="transparent"
+              label="Notifications"
+              isLabelHidden
+            />
+          </div>
+        </dialog>
+
+        <BurgerButton
+          className="header__burger-button visible-tablet"
+          extraAttrs={{
+            "data-js-overlay-burger-button": "",
+          }}
+        />
       </div>
     </header>
   )
